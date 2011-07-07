38b72a386c7dc210a11a42adfebc8b80f0dbe3de
zollarsd <zollarsd@163e820d-e23f-42e5-a746-3bfd4f3d8f09>
2011-07-07 19:20:45 +0000

    Initial import

---
+++ trunk/partners.php
@@ -0,0 +1,24 @@
+<?php
+include 'header.php';
+?>
+<div id='content'>
+	<h1 class="title">UltraScan Partners:</h1>
+   The <b><i>UltraScan</i></b> TeraGrid Science Gateway and LIMS system is supported by the
+   following Institutions:
+   <table cellspacing='0px' cellpadding='0px' class='imagelinks'>
+     <tr><td><img src='images/teragrid.png'></td>
+         <td><img src='images/TACC.gif'></td></tr>
+     <tr><td><a href='http://www.teragrid.org/'>Teragrid</a></td>
+         <td><a href='http://www.tacc.utexas.edu'>
+         Texas Advanced<br />Computing Center</a></td></tr>
+     <tr><td style='padding-top:2em;'><img src='images/uthscsa.gif'></td>
+         <td><img src='images/iu.png'></td></tr>
+     <tr><td><a href='http://www.uthscsa.edu'>
+         The University of Texas<br />Health Science Center</a></td>
+         <td><a href=http://www.iu.edu/>Indiana University</a></td></tr>
+  </table>
+
+</div>
+
+
+<?php include 'footer.php'; ?>

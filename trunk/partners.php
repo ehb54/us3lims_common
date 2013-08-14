6fbb01bb139b081a0540e264d99424145376845e
gegorbet <gegorbet@163e820d-e23f-42e5-a746-3bfd4f3d8f09>
2013-08-14 16:24:17 +0000

    various refinements, including updated wiki.bcf url

--- trunk/partners.php
+++ trunk/partners.php
@@ -5,9 +5,14 @@ include 'header.php';
 ?>
 <div id='content'>
 	<h1 class="title">UltraScan Partners:</h1>
-   The <b><i>UltraScan</i></b> TeraGrid Science Gateway and LIMS system is supported by the
+   The <b>UltraScan</b> XSEDE Science Gateway and LIMS system is supported by the
    following Institutions:
+<p/>
    <table cellspacing='0px' cellpadding='0px' class='imagelinks'>
+     <tr><td><img src='images/xsede.png' alt='XSEDE Logo' /></td>
+         <td><img src='images/sdsc_logo.png' alt='SDSC Logo' /></td></tr>
+     <tr><td><a href='http://www.xsede.org/'>XSEDE</a></td>
+         <td><a href='http://www.sdsc.edu'>SDSC</a></td></tr>
      <tr><td><img src='images/teragrid.png' alt='Teragrid Logo' /></td>
          <td><img src='images/TACC.gif' alt='TACC Logo' /></td></tr>
      <tr><td><a href='http://www.teragrid.org/'>Teragrid</a></td>

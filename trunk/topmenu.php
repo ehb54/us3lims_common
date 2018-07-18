e8592efda1082bf5705da5931b5968736695ba1b
gegorbet <gegorbet@163e820d-e23f-42e5-a746-3bfd4f3d8f09>
2018-07-18 18:58:22 +0000

    hsc-to-aucsolutions mods

--- trunk/topmenu.php
+++ trunk/topmenu.php
@@ -1,30 +1,30 @@
    <div id="globalnav">
       <ul class='level1'>
-       	<li><a href="http://ultrascan.uthscsa.edu/index.php">Home</a></li>
-	<li><a href='http://www.ultrascan3.uthscsa.edu/index.php'>UltraScan III</a></li>
-	<li><a href='http://www.ultrascan2.uthscsa.edu/index.php'>UltraScan II</a></li>
-	<li class='submenu'><a href='http://www.uslims.uthscsa.edu'>LIMS</a>
-	<ul class='level2'>
-		<li><a href="http://www.uslims.uthscsa.edu/">Main Website</a></li>
-       		<li><a href="http://www.uslims3.uthscsa.edu/index.php">LIMS-III</a></li>
-       		<li><a href="http://www.uslims2.uthscsa.edu/index.php">LIMS-II</a></li>
-	</ul></li>
-        <li class='submenu'><a href='http://www.somo.uthscsa.edu/'>SOMO</a>
-        <ul class='level2'>
-		<li><a href='http://www.beads.uthscsa.edu/'>Beadmodeling</a></li>
-		<li><a href='http://www.sas.uthscsa.edu/'>SAXS/SANS</a></li>
-		<li><a href='http://www.dynamics.uthscsa.edu/'>Mol. Dynamics</a></li>
-        </ul></li>
-        <li class='submenu'><a href='http://wiki.bcf2.uthscsa.edu/'>Wiki</a>
-        <ul class='level2'>
-		<li><a href='http://wiki.bcf2.uthscsa.edu/ultrascan/'>UltraScan-II</a></li>
-		<li><a href='http://wiki.bcf2.uthscsa.edu/cauma/'>LIMS-II</a></li>
-		<li><a href='http://wiki.bcf2.uthscsa.edu/ultrascan3/'>UltraScan-III</a></li>
-		<li><a href='http://wiki.bcf2.uthscsa.edu/limsv3/'>LIMS-III</a></li>
-		<li><a href='http://wiki.bcf2.uthscsa.edu/somo/'>US SOMO</a></li>
-		<li><a href='http://wiki.bcf2.uthscsa.edu/dmd/'>DMD</a></li>
-		<li><a href='http://wiki.bcf2.uthscsa.edu/cfa/'>CFA</a></li>
-		<li><a href='http://wiki.bcf2.uthscsa.edu/aucmanual/'>AUC Manual</a></li>
-         </ul></li>
+          <li><a href="http://ultrascan.aucsolutions.com/index.php">Home</a></li>
+   <li><a href='http://www.ultrascan3.aucsolutions.com/index.php'>UltraScan III</a></li>
+   <li><a href='http://www.ultrascan2.aucsolutions.com/index.php'>UltraScan II</a></li>
+   <li class='submenu'><a href='http://www.uslims.aucsolutions.com'>LIMS</a>
+   <ul class='level2'>
+      <li><a href="http://www.uslims3.latrobe.edu.au/">Main Website</a></li>
+        <li><a href="http://uslims3.latrobe.edu.au/index.php">LIMS-III</a></li>
+        <li><a href="http://www.uslims2.aucsolutions.com/index.php">LIMS-II</a></li>
+   </ul></li>
+      <li class='submenu'><a href='http://www.somo.aucsolutions.com/'>SOMO</a>
+      <ul class='level2'>
+        <li><a href='http://www.beads.aucsolutions.com/'>Beadmodeling</a></li>
+        <li><a href='http://www.sas.aucsolutions.com/'>SAXS/SANS</a></li>
+        <li><a href='http://www.dynamics.aucsolutions.com/'>Mol. Dynamics</a></li>
+      </ul></li>
+      <li class='submenu'><a href='http://wiki.bcf2.aucsolutions.com/'>Wiki</a>
+      <ul class='level2'>
+        <li><a href='http://wiki.aucsolutions.com/ultrascan/'>UltraScan-II</a></li>
+        <li><a href='http://wiki.aucsolutions.com/cauma/'>LIMS-II</a></li>
+        <li><a href='http://wiki.aucsolutions.com/ultrascan3/'>UltraScan-III</a></li>
+        <li><a href='http://wiki.aucsolutions.com/limsv3/'>LIMS-III</a></li>
+        <li><a href='http://wiki.aucsolutions.com/somo/'>US SOMO</a></li>
+        <li><a href='http://wiki.aucsolutions.com/dmd/'>DMD</a></li>
+        <li><a href='http://wiki.aucsolutions.com/cfa/'>CFA</a></li>
+        <li><a href='http://wiki.aucsolutions.com/aucmanual/'>AUC Manual</a></li>
+      </ul></li>
       </ul>
    </div>

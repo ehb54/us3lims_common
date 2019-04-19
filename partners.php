d006eb0f7d7415b2a8b370450a52e39619fbb004
Gary E. Gorbet <gegorbet@gmail.com>
2019-04-19 03:10:36 +0000

    adds and fixes to the Partners page of LIMS

--- partners.php
+++ partners.php
@@ -18,15 +18,22 @@ include 'header.php';
      <tr><td><a href='http://www.teragrid.org/'>Teragrid</a></td>
          <td><a href='http://www.tacc.utexas.edu'>
          Texas Advanced<br />Computing Center</a></td></tr>
-     <tr><td style='padding-top:2em;'><img src='images/uthscsa.gif' alt='UTHSCSA Logo' /></td>
-         <td><img src='images/iu.png' alt='IU Logo' /></td></tr>
-     <tr><td><a href='http://www.uthscsa.edu'>
-         The University of Texas<br />Health Science Center</a></td>
-         <td><a href='http://www.iu.edu/'>Indiana University</a></td></tr>
      <tr><td><img src='images/prace_logo.png' alt='PRACE Logo' /></td>
-         <td></td></tr>
+         <td><img src='images/iu.png' alt='IU Logo' /></td></tr>
      <tr><td><a href='http://www.prace-ri.eu/'>
-         Partnership for Advanced<br/>Computing in Europe</a></td></tr>
+         Partnership for Advanced<br/>Computing in Europe</a></td>
+         <td><a href='http://www.iu.edu/'>Indiana University</a></td></tr>
+     <tr><td style='padding-top:2em;'><img src='images/uthscsa_logo.png' alt='UTHSCSA Logo' /></td></tr>
+     <tr><td><a href='http://www.uthscsa.edu'>
+         The University of Texas<br/>Health Science Center</a></td></tr>
+     <tr><td><img src='images/UofMont_logo.png' alt='UofMontana Logo' /></td>
+         <td><img src='images/UofLeth_logo.png' alt='UofLethbridge Logo' /></td></tr>
+     <tr><td><a href='http://www.umt.edu'>University of Montana</a></td>
+         <td><a href='http://www.uleth.ca'>University of Lethbridge</a></td></tr>
+     <tr><td><img src='images/nbc_uleth_logo.png' alt='NWBiophyConsort Logo' /></td>
+         <td><img src='images/cch_uleth_logo.png' alt='CanCentHydrodyn Logo' /></td></tr>
+     <tr><td><a href='http://nbc.uleth.ca'>Northwest Biophysics Consortium</a></td>
+         <td><a href='http://cch.uleth.ca'>Canadian Center for Hydrodynamics</a></td></tr>
   </table>
 
 </div>
